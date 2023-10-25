e1b28becd453a416ba8e89b3515e72e4518043b5
Mihndim <mihndim2016@gmail.com>
2023-10-25 11:51:31 +0100

    Add environment variables

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     res.set({
         'Content-Type': 'application/json',
         'sid': 'f614e8bf-aecf-31b9-854c-a143ca774ca6',
-        'auth': 'H+WKgGwd6C37yjiMrTPzWG0i29LQyNWkc5EgwhMdjPB7fRZ4IqZ8AjJMQOfAEm5q/PjPN31HlA4RV/t1AmsUXidB5mP0AR6E3VCXEWw9T9n43oWldAFGSDoxn1IBtf/DE0icyrvD+j043ZVc8Ha3PHytZ3mEwMypQHDrM706hMtdJuBA1W3OosEEkC+J7orHKibD2PW36nLd8CLWW8+VEalfmNNwhbeBHHUW7WjcYYfoL60d9OBGxbnr6zrhb54NMUEnMPC3anUnua3q/46o1uA5FyTCTm46OUHUfiSLRnDduTsAUQERZYnPvd7M36kd37if4Uf6Cqi2V7E44CDfRQ==',
+        'auth': `${process.env.auth}`,
       })
     next();
 });
