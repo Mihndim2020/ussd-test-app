41c421390ad12b69c8b9a1254b99d8faef443410
Mihndim <mihndim2016@gmail.com>
2023-11-01 13:42:49 +0100

    Update http method

--- controllers/ussd_pages.js
+++ controllers/ussd_pages.js
@@ -5,7 +5,7 @@ module.exports.check_bill = async (req, res) => {
     let message = "";
     const userEntry = req.headers["user-entry"];
     try {
-       const response = await axios.post(`https://core.diool.com/core/onlinepayment/v1/payment/${userEntry}`, {
+       const response = await axios.get(`https://core.diool.com/core/onlinepayment/v1/payment/${userEntry}`, {
         headers: {
             Authorization: `${process.env.checkBillToken}`,
         }
