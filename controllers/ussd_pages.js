a42d61bc7d2ac76a08601418a0cbca6eb671d4e2
Mihndim <mihndim2016@gmail.com>
2023-11-15 15:17:29 +0100

    Update app to make it dynamic

--- controllers/ussd_pages.js
+++ controllers/ussd_pages.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
 const axios = require("axios");
+const Cache = require("node-cache");
+const ussdCache = new Cache({ stdTTL: 100, deleteOnExpire: true, checkperiod: 120 });
 
 module.exports.check_bill = async (req, res) => {
-    let message = "";
+    let userMsisdn = req.headers["user-msisdn"]; // We will add a default value here for test in case the value is not provided by Myriad in the request headers.
+
+    if (!userMsisdn) {
+        userMsisdn = "237677999999";
+    }
+
     const userEntry = req.headers["user-entry"];
+
+       // Set the value in cache. 
+    ussdCache.set(userMsisdn, userEntry); // We have to look for a way to make this unique... 
+
+    console.log(ussdCache.get(userMsisdn));
+
+    let message = "";
+
     try {
-       const response = await axios.get(`https://core.diool.com/core/onlinepayment/v1/payment/${userEntry}`, {
+       const response = await axios.get(`${process.env.paymentUrl}/${userEntry}`, {
         headers: {
             "Authorization": `${process.env.checkBillToken}`,
             "Content-Type": "application/json",
@@ -15,15 +30,14 @@ module.exports.check_bill = async (req, res) => {
        const data = await response.data;
 
        if (data.code === 404) {
-        message = "Oups, the bill/tax number entered can not be found, please check the number and try again";
-        res.json({
-            "title":"Mairie de Douala 5ème",
+        message = "Oops, the bill number entered is incorrect. Please check the bill number and try again";
+        return res.json({
+            "title":`${process.env.pageTitle}`,
             "name": "Diool Bill payments",
             "message":`${message}`,
-            // "message":"Please enter your bill/tax number: ",
             "form": {
-               // "url": `${process.env.baseUrl}/payorexit`,
-                "url": "https://cad5-ussd.onrender.com/payorexit",
+               "url": `${process.env.baseUrl}/payorexit`,
+               //"url": "https://cad5-ussd.onrender.com/payorexit",
                 "type": "text",
                 "method": "get"
             },
@@ -35,23 +49,34 @@ module.exports.check_bill = async (req, res) => {
         })
        }
 
+       if (data.code === 0 && data.result.status === "PENDING_PAYMENT" && data.result.amount > 1000000 ) {
+            message = `Amount is not authorized by this payment method. Please call 650 774 040 for instructions on how to pay`;
+
+            return res.json({
+                "page":{
+                    "session_end":"true"
+                },
+                "message": `${message}`
+            });
+       }
+
         if (data.code === 0 && data.result.status === "PENDING_PAYMENT") {
-        message = `M./Ms ${data.result.recipient.lastName}, your bill ${data.result.referenceId} of amount: ${data.result.amount} is pending payment. Please you have until ${new Date(data.result.expiresOn).toLocaleDateString()} to settle your bill`;
+        message = `Bill From: ${data.result.sender.businessName}, \n Bill To: ${data.result.recipient.firstName}, \n Amount: ${data.result.amount}, \n For: ${data.result.paymentFor}, \n Pay before: ${new Date(data.result.expiresOn).toLocaleDateString()}`;
 
         res.json({
-            "title":"Mairie de Douala 5ème",
+          "title":`${process.env.pageTitle}`,
           "name": "Diool Bill payments",
             "message":`${message}`,
             "links": [
                 {
-                "content":" Pay my tax",
-               // "url":`${process.env.baseUrl}/enterphonetopay`
-                "url":"https://cad5-ussd.onrender.com/choosetelco"
+                "content":" Pay my Bill",
+                "url":`${process.env.baseUrl}/choosetelco`
+                //"url":"https://cad5-ussd.onrender.com/choosetelco"
                 },
                 {
                 "content":" To Quit",
-               // "url":`${process.env.baseUrl}/quit` 
-                "url":"https://cad5-ussd.onrender.com/quit" 
+                "url":`${process.env.baseUrl}/quit` 
+                //"url":"https://cad5-ussd.onrender.com/quit" 
                 }
             ],
             "page":{
@@ -63,8 +88,9 @@ module.exports.check_bill = async (req, res) => {
 
        } 
 
-       if (data.code === 0 && data.result.status === "PAID" ) {
-        message = `M./Ms ${data.result.recipient.lastName}, your bill ${data.result.referenceId} of amount: ${data.result.amount} is has been paid. Thank you for using Diool`;
+       if (data.code === 0 && (data.result.status === "PAID" || data.result.status === "EXPIRED" || data.result.status === "CANCELLED" || data.result.status === "DRAFT") ) {
+        message = `The bill with reference: ${data.result.referenceId} of amount: ${data.result.amount} XAF is ${data.result.status}. Thank you for using Diool Bills Payment`;
+
         res.json({
             "page":{
                 "session_end":"true"
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require("express");
 const ussdPages = require("../controllers/ussd_pages");
+const Cache = require("node-cache");
+const ussdCache = new Cache({ stdTTL: 100, deleteOnExpire: true, checkperiod: 120 });
 // set the routes as environmental variables... 
 
 const router = express.Router();
@@ -8,24 +10,19 @@ const router = express.Router();
 router.get("/", (req, res) => {
     console.log(req.headers.host);
     res.json({
-        "title":"Mairie de Douala 5ème",
+        "title":`${process.env.pageTitle}`,
       "name": "Diool Bill payments",
-        "message":"Welcome to CAD 5, please enter: ",
+        "message":"Please enter: ",
         "links": [
             {
-            "content":" To Pay your tax",
-           // "url":`${process.env.baseUrl}/enterbilltocheck`
-            "url":"https://cad5-ussd.onrender.com/enterbilltocheck"
+            "content":" To Pay your Bill",
+            "url":`${process.env.baseUrl}/enterbilltocheck`
+           // "url":"https://cad5-ussd.onrender.com/enterbilltocheck"
             },
             {
-            "content":" To Check your tax status",
-           // "url":`${process.env.baseUrl}/enterbilltocheck`
-            "url":"https://cad5-ussd.onrender.com/enterpaidbill"
-            },
-            {
-            "content":" Make a deposit / pay upfront",
-           // "url":`${process.env.baseUrl}/enterbilltocheck`
-            "url":"https://cad5-ussd.onrender.com/enterpayupfront"
+            "content":" To Check the Status of your Bill",
+            "url":`${process.env.baseUrl}/enterbilltocheck`
+           //"url":"https://cad5-ussd.onrender.com/enterpaidbill"
             }
         ],
         "page":{
@@ -36,71 +33,15 @@ router.get("/", (req, res) => {
     })
 })
 
-router.get("/enterpayupfront", (req, res) => {
-    res.json({
-        "title":"Mairie de Douala 5ème",
-      "name": "Diool Bill payments",
-        "message":"Please enter your bill/tax number: ",
-        "form": {
-           // "url": `${process.env.baseUrl}/payorexit`,
-            "url": "https://cad5-ussd.onrender.com/enteramount",
-            "type": "text",
-            "method": "get"
-        },
-        "page":{
-            "menu":"true",
-            "history":"true",
-            "navigation_keywords":"true"
-        }
-    })
-})
-
-router.get("/enteramount", (req, res) => {
-    res.json({
-        "title":"Mairie de Douala 5ème",
-      "name": "Diool Bill payments",
-        "message":"Please enter amount: ",
-        "form": {
-           // "url": `${process.env.baseUrl}/payorexit`,
-            "url": "https://cad5-ussd.onrender.com/choosetelco",
-            "type": "text",
-            "method": "get"
-        },
-        "page":{
-            "menu":"true",
-            "history":"true",
-            "navigation_keywords":"true"
-        }
-    })
-})
-
-router.get("/enterpaidbill", (req, res) => {
-    res.json({
-        "title":"Mairie de Douala 5ème",
-      "name": "Diool Bill payments",
-        "message":"Please enter your bill/tax number: ",
-        "form": {
-           // "url": `${process.env.baseUrl}/payorexit`,
-            "url": "https://cad5-ussd.onrender.com/billpaid",
-            "type": "text",
-            "method": "get"
-        },
-        "page":{
-            "menu":"true",
-            "history":"true",
-            "navigation_keywords":"true"
-        }
-    })
-})
 
 router.get("/enterbilltocheck", (req, res) => {
     res.json({
-        "title":"Mairie de Douala 5ème",
-      "name": "Diool Bill payments",
-        "message":"Please enter your bill/tax number: ",
+        "title":`${process.env.pageTitle}`,
+        "name": "Diool Bill payments",
+        "message":"Please enter your Bill reference: ",
         "form": {
-           // "url": `${process.env.baseUrl}/payorexit`,
-            "url": "https://cad5-ussd.onrender.com/payorexit",
+            "url": `${process.env.baseUrl}/payorexit`,
+           // "url": "https://cad5-ussd.onrender.com/payorexit",
             "type": "text",
             "method": "get"
         },
@@ -114,51 +55,26 @@ router.get("/enterbilltocheck", (req, res) => {
 
 router.get("/payorexit", ussdPages.check_bill );
 
-// router.get("/payorexit", async (req, res) => {
-//     res.json({
-//         "title":"Mairie de Douala 5ème",
-//       "name": "Diool Bill payments",
-//         "message":`M./Ms XYZ, your tax number ${Math.floor(100000000 + Math.random() * 900000000)} of amount ${Math.floor(100000 + Math.random() * 900000)} XAF  is available for payment: `,
-//         "links": [
-//             {
-//             "content":" Pay my tax",
-//            // "url":`${process.env.baseUrl}/enterphonetopay`
-//             "url":"https://cad5-ussd.onrender.com/choosetelco"
-//             },
-//             {
-//             "content":" To Quit",
-//            // "url":`${process.env.baseUrl}/quit` 
-//             "url":"https://cad5-ussd.onrender.com/quit" 
-//             }
-//         ],
-//         "page":{
-//             "menu":"true",
-//             "history":"true",
-//             "navigation_keywords":"true"
-//         }
-//     })
-// })
-
 router.get("/choosetelco", (req, res) => {
     res.json({
-        "title":"Mairie de Douala 5ème",
+      "title":`${process.env.pageTitle}`,
       "name": "Diool Bill payments",
         "message":"Choose your mobile money operator: ",
         "links": [
             {
             "content":" Orange Money",
-           // "url":`${process.env.baseUrl}/enterphonetopay`
-            "url":"https://cad5-ussd.onrender.com/enterphonetopay"
+            "url":`${process.env.baseUrl}/enterphonetopay`
+            //"url":"https://cad5-ussd.onrender.com/enterphonetopay"
             },
             {
             "content":" MTN Mobile Money",
-           // "url":`${process.env.baseUrl}/enterphonetopay`
-            "url":"https://cad5-ussd.onrender.com/enterphonetopay"
+            "url":`${process.env.baseUrl}/enterphonetopay`
+            //"url":"https://cad5-ussd.onrender.com/enterphonetopay"
             },
             {
             "content":" Express Union Mobile Money",
-           // "url":`${process.env.baseUrl}/enterphonetopay`
-            "url":"https://cad5-ussd.onrender.com/enterphonetopay"
+            "url":`${process.env.baseUrl}/enterphonetopay`
+            //"url":"https://cad5-ussd.onrender.com/enterphonetopay"
             }
         ],
         "page":{
@@ -170,13 +86,26 @@ router.get("/choosetelco", (req, res) => {
 })
 
 router.get("/enterphonetopay", (req, res) => {
+
+    let paymentMethod = req.headers["user-entry"];
+
+    if (paymentMethod === 1) {
+        paymentMethod = "62401"
+    } else if (paymentMethod === 2) {
+        paymentMethod = "62402"
+    } else {
+        paymentMethod = "EUMM"
+    }
+
+    ussdCache.set(providerCode, paymentMethod); // We have to link this to a session... We need unique keys, we don't know if there is a possibility of serveral copies existing at the same time. 
+
     res.json({
-    "title":"Mairie de Douala 5ème",
+       "title":`${process.env.pageTitle}`,
        "name": "Diool Bill payments",
         "message":"Please enter the phone number you want to pay with: ",
         "form": {
-           // "url": `${process.env.baseUrl}/paymentrequest`,
-            "url": "https://cad5-ussd.onrender.com/paymentrequest",
+            "url": `${process.env.baseUrl}/paymentrequest`,
+           //"url": "https://cad5-ussd.onrender.com/paymentrequest",
             "type": "text",
             "method": "get"
         },
@@ -188,21 +117,61 @@ router.get("/enterphonetopay", (req, res) => {
     })
 });
 
-router.get("/billpaid", (req, res) => {
-    res.json({
-        "page":{
-            "session_end":"true"
-        },
-        "message":`Your tax number ${Math.floor(100000000 + Math.random() * 900000000)} of amount ${Math.floor(100000 + Math.random() * 900000)} XAF has been paid. Thank you for using our services ` // customize messages will be useful here. 
-    })
-});
 router.get("/paymentrequest", (req, res) => {
-    res.json({
-        "page":{
-            "session_end":"true"
-        },
-        "message":"Please validate the payment request from your mobile money operator, to complete the payment" // customize messages will be useful here. 
-    })
+
+    const userMsisdn = req.headers["user-msisdn"]; // We need to look for way to uniquely identify a cache value if msisdn is not provided... 
+    const phoneNumber = req.headers["user-entry"]; // Value to be used in later implementations, using aliases as unique identifiers.. 
+
+    // if (!userMsisdn) {
+    //     userMsisdn = "237677999999";
+    // }
+
+    const rfpReference = ussdCache.get(userMsisdn);
+    console.log("The rfp referene is:",  ussdCache.get(userMsisdn));
+
+    const sendPaymentRequest = async () => {
+
+        const response = await axios.post(`https://core.diool.me/core/onlinepayment/v1/payRfp`, {
+              "providerAccountID": `237${phoneNumber}`,
+              "providerCode": `${ussdCache.get(providerCode)}`, // Checking the telco still to be implemented. 
+              "requestPaymentReference": rfpReference 
+          }, {
+              headers: {
+                  "Authorization": `${process.env.checkBillToken}`,
+                  "Content-Type": "application/json",
+                  "X-Beversion": "4.0.0"
+              }
+          });
+  
+          const data = await response.data;
+          console.log(data);
+      }
+
+      try {
+
+        setTimeout(() => {
+            console.log("The sendPaymentRequest function was called ! ");
+            sendPaymentRequest();
+        }, 1000);
+
+        return res.json({
+            "page":{
+                "session_end":"true"
+            },
+            "message": "Your request is being processed. To finalize your transaction, please dial *126# and enter your PIN code" // customize messages will be useful here. 
+        }); // Message to be customized based on the Telco.... 
+        
+    } catch (error) {
+
+        return res.json( {
+            "page":{
+                "session_end":"true"
+            },
+            //"message": `${error.message}`
+            "message": `We were faced with an error while processing your request, please try again later`
+        });
+
+    }
 });
 
 router.get("/quit", (req, res) => {
@@ -210,7 +179,7 @@ router.get("/quit", (req, res) => {
         "page":{
             "session_end":"true"
         },
-        "message":"Thanks for using our service"
+        "message":"Thanks for using Diool Bill Payments"
     })
 });
 
