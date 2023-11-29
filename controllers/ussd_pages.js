ec86fbabbae6823457d350dee47c73955e48b088
Mihndim <mihndim2016@gmail.com>
2023-11-29 12:52:56 +0100

    Fix reference error

--- controllers/ussd_pages.js
+++ controllers/ussd_pages.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const axios = require("axios");
 //const Cache = require("node-cache");
 const refCache = require("../cache");
@@ -6,106 +6,122 @@ const refCache = require("../cache");
 //const refCache = new Cache({ stdTTL: 120, deleteOnExpire: true, checkperiod: 120 });
 
 module.exports.check_bill = async (req, res) => {
-   // let userMsisdn = req.headers["user-msisdn"]; // We will add a default value here for test in case the value is not provided by Myriad in the request headers.
+  // let userMsisdn = req.headers["user-msisdn"]; // We will add a default value here for test in case the value is not provided by Myriad in the request headers.
 
-    const userEntry = req.headers["user-entry"];
+  const userEntry = req.headers["user-entry"];
 
-       // Set the value in cache. 
-    refCache.set("uniqueReference", userEntry, 120); // We have to look for a way to make this unique... 
+  // Set the value in cache.
+  refCache.set("uniqueReference", userEntry, 120); // We have to look for a way to make this unique...
 
-    console.log("The uniqueReferenece is like: ", refCache.get("uniqueReference"));
+  console.log(
+    "The uniqueReferenece is like: ",
+    refCache.get("uniqueReference")
+  );
 
-    let message = "";
+  let message = "";
 
-    try {
-       const response = await axios.get(`${process.env.paymentUrl}/${userEntry}`, {
-        headers: {
-            "Authorization": `${process.env.checkBillToken}`,
-            "Content-Type": "application/json",
-            "X-Beversion": "4.0.0"
-        }
+  try {
+    const response = await axios.get(`${process.env.paymentUrl}/${userEntry}`, {
+      headers: {
+        Authorization: `${process.env.checkBillToken}`,
+        "Content-Type": "application/json",
+        "X-Beversion": "4.0.0",
+      },
     });
-       const data = await response.data;
+    const data = await response.data;
 
-       if (data.code === 404) {
-        message = "Oops, the bill number entered is incorrect. Please check the bill number and try again";
-        return res.json({
-            "title":`${process.env.pageTitle}`,
-            "name": "Diool Bill payments",
-            "message":`${message}`,
-            "form": {
-               "url": `${process.env.baseUrl}/payorexit`,
-               //"url": "https://cad5-ussd.onrender.com/payorexit",
-                "type": "text",
-                "method": "get"
-            },
-            "page":{
-                "menu":"true",
-                "history":"true",
-                "navigation_keywords":"true"
-            }
-        })
-       }
-
-       if (data.code === 0 && data.result.status === "PENDING_PAYMENT" && data.result.amount > 1000000 ) {
-            message = `Amount is not authorized by this payment method. Please call 650 774 040 for instructions on how to pay`;
-
-            return res.json({
-                "page":{
-                    "session_end":"true"
-                },
-                "message": `${message}`
-            });
-       }
-
-        if (data.code === 0 && data.result.status === "PENDING_PAYMENT") {
-        message = `Bill From: ${data.result.sender.businessName}, \n Bill To: ${data.result.recipient.firstName}, \n Amount: ${data.result.amount}, \n For: ${data.result.paymentFor}, \n Pay before: ${new Date(data.result.expiresOn).toLocaleDateString()}`;
-
-        res.json({
-          "title":`${process.env.pageTitle}`,
-          "name": "Diool Bill payments",
-            "message":`${message}`,
-            "links": [
-                {
-                "content":" Pay my Bill",
-                "url":`${process.env.baseUrl}/choosetelco`
-                //"url":"https://cad5-ussd.onrender.com/choosetelco"
-                },
-                {
-                "content":" To Quit",
-                "url":`${process.env.baseUrl}/quit` 
-                //"url":"https://cad5-ussd.onrender.com/quit" 
-                }
-            ],
-            "page":{
-                "menu":"true",
-                "history":"true",
-                "navigation_keywords":"true"
-            }
-        })
-
-       } 
-
-       if (data.code === 0 && (data.result.status === "PAID" || data.result.status === "EXPIRED" || data.result.status === "CANCELLED" || data.result.status === "DRAFT") ) {
-        message = `The bill with reference: ${data.result.referenceId} of amount: ${data.result.amount} XAF is ${data.result.status}. Thank you for using Diool Bills Payment`;
-
-        res.json({
-            "page":{
-                "session_end":"true"
-            },
-            "message":`${message}`, // customize messages will be useful here. 
-        })  
-       }
-        
-  
-    } catch (error) {
-        res.json({
-            "page":{
-                "session_end":"true"
-            },
-            "message": `${error.message}`
-            //"message": `We faced with an error while processing your request, please try again later`
-        }) 
+    if (data.code === 404) {
+      message =
+        "Oops, the bill number entered is incorrect. Please check the bill number and try again";
+      return res.json({
+        title: `${process.env.pageTitle}`,
+        name: "Diool Bill payments",
+        message: `${message}`,
+        form: {
+          url: `${process.env.baseUrl}/payorexit`,
+          //"url": "https://cad5-ussd.onrender.com/payorexit",
+          type: "text",
+          method: "get",
+        },
+        page: {
+          menu: "true",
+          history: "true",
+          navigation_keywords: "true",
+        },
+      });
     }
-} 
 
+    if (
+      data.code === 0 &&
+      data.result.status === "PENDING_PAYMENT" &&
+      data.result.amount > 1000000
+    ) {
+      message = `Amount is not authorized by this payment method. Please call 650 774 040 for instructions on how to pay`;
+
+      return res.json({
+        page: {
+          session_end: "true",
+        },
+        message: `${message}`,
+      });
+    }
+
+    if (data.code === 0 && data.result.status === "PENDING_PAYMENT") {
+      message = `Bill From: ${data.result.sender.businessName}, \n Bill To: ${
+        data.result.recipient.firstName
+      }, \n Amount: ${data.result.amount}, \n For: ${
+        data.result.paymentFor
+      }, \n Pay before: ${new Date(
+        data.result.expiresOn
+      ).toLocaleDateString()}`;
+
+      res.json({
+        title: `${process.env.pageTitle}`,
+        name: "Diool Bill payments",
+        message: `${message}`,
+        links: [
+          {
+            content: " Pay my Bill",
+            url: `${process.env.baseUrl}/choosetelco`,
+            //"url":"https://cad5-ussd.onrender.com/choosetelco"
+          },
+          {
+            content: " To Quit",
+            url: `${process.env.baseUrl}/quit`,
+            //"url":"https://cad5-ussd.onrender.com/quit"
+          },
+        ],
+        page: {
+          menu: "true",
+          history: "true",
+          navigation_keywords: "true",
+        },
+      });
+    }
+
+    if (
+      data.code === 0 &&
+      (data.result.status === "PAID" ||
+        data.result.status === "EXPIRED" ||
+        data.result.status === "CANCELLED" ||
+        data.result.status === "DRAFT")
+    ) {
+      message = `The bill with reference: ${data.result.referenceId} of amount: ${data.result.amount} XAF is ${data.result.status}. Thank you for using Diool Bills Payment`;
+
+      res.json({
+        page: {
+          session_end: "true",
+        },
+        message: `${message}`, // customize messages will be useful here.
+      });
+    }
+  } catch (error) {
+    res.json({
+      page: {
+        session_end: "true",
+      },
+      message: `${error.message}`,
+      //"message": `We faced with an error while processing your request, please try again later`
+    });
+  }
+};
--- routes/router.js
+++ routes/router.js
@@ -1,197 +1,191 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const ussdPages = require("../controllers/ussd_pages");
 const ussdCache = require("../cache");
 //const Cache = require("node-cache");
 //const ussdCache = new Cache({ stdTTL: 120, deleteOnExpire: true, checkperiod: 120 });
-// set the routes as environmental variables... 
+// set the routes as environmental variables...
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    console.log(req.headers.host);
-    res.json({
-        "title":`${process.env.pageTitle}`,
-      "name": "Diool Bill payments",
-        "message":"Please enter: ",
-        "links": [
-            {
-            "content":" To Pay your Bill",
-            "url":`${process.env.baseUrl}/enterbilltocheck`
-           // "url":"https://cad5-ussd.onrender.com/enterbilltocheck"
-            },
-            {
-            "content":" To Check the Status of your Bill",
-            "url":`${process.env.baseUrl}/enterbilltocheck`
-           //"url":"https://cad5-ussd.onrender.com/enterpaidbill"
-            }
-        ],
-        "page":{
-            "menu":"true",
-            "history":"true",
-            "navigation_keywords":"true"
-        }
-    })
-})
-
+  console.log(req.headers.host);
+  res.json({
+    title: `${process.env.pageTitle}`,
+    name: "Diool Bill payments",
+    message: "Please enter: ",
+    links: [
+      {
+        content: " To Pay your Bill",
+        url: `${process.env.baseUrl}/enterbilltocheck`,
+        // "url":"https://cad5-ussd.onrender.com/enterbilltocheck"
+      },
+      {
+        content: " To Check the Status of your Bill",
+        url: `${process.env.baseUrl}/enterbilltocheck`,
+        //"url":"https://cad5-ussd.onrender.com/enterpaidbill"
+      },
+    ],
+    page: {
+      menu: "true",
+      history: "true",
+      navigation_keywords: "true",
+    },
+  });
+});
 
 router.get("/enterbilltocheck", (req, res) => {
-    res.json({
-        "title":`${process.env.pageTitle}`,
-        "name": "Diool Bill payments",
-        "message":"Please enter your Bill reference: ",
-        "form": {
-            "url": `${process.env.baseUrl}/payorexit`,
-           // "url": "https://cad5-ussd.onrender.com/payorexit",
-            "type": "text",
-            "method": "get"
-        },
-        "page":{
-            "menu":"true",
-            "history":"true",
-            "navigation_keywords":"true"
-        }
-    })
-})
+  res.json({
+    title: `${process.env.pageTitle}`,
+    name: "Diool Bill payments",
+    message: "Please enter your Bill reference: ",
+    form: {
+      url: `${process.env.baseUrl}/payorexit`,
+      // "url": "https://cad5-ussd.onrender.com/payorexit",
+      type: "text",
+      method: "get",
+    },
+    page: {
+      menu: "true",
+      history: "true",
+      navigation_keywords: "true",
+    },
+  });
+});
 
-router.get("/payorexit", ussdPages.check_bill );
+router.get("/payorexit", ussdPages.check_bill);
 
 router.get("/choosetelco", (req, res) => {
-    res.json({
-      "title":`${process.env.pageTitle}`,
-      "name": "Diool Bill payments",
-        "message":"Choose your mobile money operator: ",
-        "links": [
-            {
-            "content":" Orange Money",
-            "url":`${process.env.baseUrl}/enterphonetopay`
-            //"url":"https://cad5-ussd.onrender.com/enterphonetopay"
-            },
-            {
-            "content":" MTN Mobile Money",
-            "url":`${process.env.baseUrl}/enterphonetopay`
-            //"url":"https://cad5-ussd.onrender.com/enterphonetopay"
-            },
-            {
-            "content":" Express Union Mobile Money",
-            "url":`${process.env.baseUrl}/enterphonetopay`
-            //"url":"https://cad5-ussd.onrender.com/enterphonetopay"
-            }
-        ],
-        "page":{
-            "menu":"true",
-            "history":"true",
-            "navigation_keywords":"true"
-        }
-    })
-})
+  res.json({
+    title: `${process.env.pageTitle}`,
+    name: "Diool Bill payments",
+    message: "Choose your mobile money operator: ",
+    links: [
+      {
+        content: " Orange Money",
+        url: `${process.env.baseUrl}/enterphonetopay`,
+        //"url":"https://cad5-ussd.onrender.com/enterphonetopay"
+      },
+      {
+        content: " MTN Mobile Money",
+        url: `${process.env.baseUrl}/enterphonetopay`,
+        //"url":"https://cad5-ussd.onrender.com/enterphonetopay"
+      },
+      {
+        content: " Express Union Mobile Money",
+        url: `${process.env.baseUrl}/enterphonetopay`,
+        //"url":"https://cad5-ussd.onrender.com/enterphonetopay"
+      },
+    ],
+    page: {
+      menu: "true",
+      history: "true",
+      navigation_keywords: "true",
+    },
+  });
+});
 
 router.get("/enterphonetopay", async (req, res) => {
+  let paymentMethod;
 
-    let paymentMethod;
+  console.log("Request headers", req.headers);
+  console.log("User Entry", req.headers["user_entry"]); // User entry is passed in as a string...
 
-    console.log("Request headers", req.headers);
-    console.log("User Entry", req.headers["user_entry"]); // User entry is passed in as a string... 
+  if (req.headers["user_entry"] === "1") {
+    paymentMethod = "62402";
+  }
+  if (req.headers["user_entry"] === "2") {
+    paymentMethod = "62401";
+  }
+  if (req.headers["user_entry"] === "3") {
+    paymentMethod = "EUMM";
+  }
 
-    
-    if (req.headers["user_entry"] === "1") {
-        paymentMethod = "62402"
-    }; 
-    if (req.headers["user_entry"] === "2") {
-        paymentMethod = "62401"
-    } 
-    if(req.headers["user_entry"] === "3") {
-        paymentMethod = "EUMM"
-    }
-    
-    console.log("Telco", paymentMethod);
+  console.log("Telco", paymentMethod);
 
-    ussdCache.set(paymentMethod, paymentMethod, 120); // We have to link this to a session... We need unique keys, we don't know if there is a possibility of serveral copies existing at the same time. 
+  ussdCache.set(paymentMethod, paymentMethod, 120); // We have to link this to a session... We need unique keys, we don't know if there is a possibility of serveral copies existing at the same time.
 
-    console.log(ussdCache.get(paymentMethod));
+  console.log(ussdCache.get(paymentMethod));
 
-    res.json({
-       "title":`${process.env.pageTitle}`,
-       "name": "Diool Bill payments",
-        "message":"Please enter the phone number you want to pay with: ",
-        "form": {
-            "url": `${process.env.baseUrl}/paymentrequest`,
-           //"url": "https://cad5-ussd.onrender.com/paymentrequest",
-            "type": "text",
-            "method": "get"
-        },
-        "page":{
-            "menu":"true",
-            "history":"true",
-            "navigation_keywords":"true"
-        }
-    })
+  res.json({
+    title: `${process.env.pageTitle}`,
+    name: "Diool Bill payments",
+    message: "Please enter the phone number you want to pay with: ",
+    form: {
+      url: `${process.env.baseUrl}/paymentrequest`,
+      //"url": "https://cad5-ussd.onrender.com/paymentrequest",
+      type: "text",
+      method: "get",
+    },
+    page: {
+      menu: "true",
+      history: "true",
+      navigation_keywords: "true",
+    },
+  });
 });
 
 router.get("/paymentrequest", (req, res) => {
+  const phoneNumber = `237${req.headers["user-entry"]}`; // Value to be used in later implementations, using aliases as unique identifiers..
 
-    const phoneNumber = `237${req.headers["user-entry"]}`; // Value to be used in later implementations, using aliases as unique identifiers.. 
+  console.log("phoneNumber", phoneNumber);
 
-    console.log("phoneNumber", phoneNumber);
+  //const rfpReference = refCache.get("uniqueReference");
+  //console.log("The rfp referene is:",  refCache.get("uniqueReference"));
 
-    const rfpReference = refCache.get("uniqueReference");
-    console.log("The rfp referene is:",  refCache.get("uniqueReference"));
-
-    const sendPaymentRequest = async () => {
-
-        const response = await axios.post(`https://core.diool.me/core/onlinepayment/v1/payRfp`, {
-              "providerAccountID": phoneNumber,
-              "providerCode": `${ussdCache.get(paymentMethod)}`, // Checking the telco still to be implemented. 
-              "requestPaymentReference": rfpReference 
-          }, {
-              headers: {
-                  "Authorization": `${process.env.checkBillToken}`,
-                  "Content-Type": "application/json",
-                  "X-Beversion": "4.0.0"
-              }
-          });
-  
-          const data = await response.data;
-          console.log(data);
+  const sendPaymentRequest = async () => {
+    const response = await axios.post(
+      `https://core.diool.me/core/onlinepayment/v1/payRfp`,
+      {
+        providerAccountID: phoneNumber,
+        providerCode: `${ussdCache.get(paymentMethod)}`, // Checking the telco still to be implemented.
+        requestPaymentReference: rfpReference,
+      },
+      {
+        headers: {
+          Authorization: `${process.env.checkBillToken}`,
+          "Content-Type": "application/json",
+          "X-Beversion": "4.0.0",
+        },
       }
+    );
 
-      try {
+    const data = await response.data;
+    console.log(data);
+  };
 
-        setTimeout(() => {
-            console.log("The sendPaymentRequest function was called ! ");
-            sendPaymentRequest();
-        }, 1000);
+  try {
+    setTimeout(() => {
+      console.log("The sendPaymentRequest function was called ! ");
+      sendPaymentRequest();
+    }, 1000);
 
-        return res.json({
-            "page":{
-                "session_end":"true"
-            },
-            "message": "Your request is being processed. To finalize your transaction, please dial *126# and enter your PIN code" // customize messages will be useful here. 
-        }); // Message to be customized based on the Telco.... 
-        
-    } catch (error) {
-
-        return res.json( {
-            "page":{
-                "session_end":"true"
-            },
-            //"message": `${error.message}`
-            "message": `We were faced with an error while processing your request, please try again later`
-        });
-
-    }
+    return res.json({
+      page: {
+        session_end: "true",
+      },
+      message:
+        "Your request is being processed. To finalize your transaction, please dial *126# and enter your PIN code", // customize messages will be useful here.
+    }); // Message to be customized based on the Telco....
+  } catch (error) {
+    return res.json({
+      page: {
+        session_end: "true",
+      },
+      //"message": `${error.message}`
+      message: `We were faced with an error while processing your request, please try again later`,
+    });
+  }
 });
 
 router.get("/quit", (req, res) => {
-    res.json({
-        "page":{
-            "session_end":"true"
-        },
-        "message":"Thanks for using Diool Bill Payments"
-    })
+  res.json({
+    page: {
+      session_end: "true",
+    },
+    message: "Thanks for using Diool Bill Payments",
+  });
 });
-
-
 
 module.exports = router;
